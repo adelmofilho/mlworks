249b8f5e4433f4891840947f2dcfb373e38fa13d
adelmofilho <adelmo.aguiar.filho@gmail.com>
2020-07-29 18:55:24 -0300

    vamo ver docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,44 +12,56 @@
 #
 # import os
 # import sys
-# sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'mlworks'
-copyright = '2020, Adelmo Filho'
-author = 'Adelmo Filho'
+project = "mlworks"
+copyright = "2020, Adelmo Filho"
+author = "Adelmo Filho"
 
 # The full version, including alpha/beta/rc tags
-release = '2020'
+release = "2020"
 
 
 # -- General configuration ---------------------------------------------------
-master_doc = 'index'
+master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
-extensions = [
+extensions = ["sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.imgmath",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx_click.ext",
+    "recommonmark"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
+# The suffix of source filenames.
+source_suffix = {'.rst': 'restructuredtext', '.md': 'markdown'}
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+
+pygments_style = 'sphinx'
+html_theme = 'sphinx_rtd_theme'
