4802757f467554a5a19816c168fddf72a77520f4
adelmofilho <adelmo.aguiar.filho@gmail.com>
2020-07-31 22:45:15 -0300

    falta fechar os metodos de nested list

--- mlworks/engineering.py
+++ mlworks/engineering.py
@@ -27,11 +27,20 @@ class Blueprint:
             plan[key]["value"] = plan[key]["value"] + value
         return plan
 
+    def __include_on_custom_list
+
+    def __include_on_order_list(plan, key, feature, order):
+        return None
+
+    def __include_on_operation_list(self, key, feature, operation):
+        return None
+
     # Original Feature
 
     def keep_original_feature(self, features):
         key = "keep_original_feature"
         self.plan = self.__include_on_list(self.plan, key, features)
+        return self
 
     # Imputation methods
 
@@ -53,4 +62,44 @@ class Blueprint:
     def impute_missing_as_number(self, features, number):
         key = "impute_missing_as_number"
         self.plan = self.__include_on_pairwise_list(self.plan, key, features, number)
+        return self
+
+    # Binning
+
+    def binning_number_one_threshold(self, features, thresholds):
+        key = "binning_number_one_threshold"
+        self.plan = self.__include_on_pairwise_list(self.plan, key, features, thresholds)
+        return self
+
+    def binning_class_one_vs_all(self, features, hot_class):
+        key = "binning_class_one_vs_all"
+        self.plan = self.__include_on_pairwise_list(self.plan, key, features, hot_class)
+        return self
+
+    def binning_one_hot_encoding(self, features):
+        key = "binning_one_hot_encoding"
+        self.plan = self.__include_on_list(self.plan, key, features)
+        return self    
+
+    def binning_add_extra_class(self, features, extra_class):
+        key = "binning_add_extra_class"
+        self.plan = self.__include_on_pairwise_list(self.plan, key, features, extra_class)
+        return self
+
+    def binning_custom_classes(self, features, extra_class):
+        key = "binning_custom_classes"
+        self.plan = self.__include_on_custom_list(self.plan, key, features, extra_class)
+        return self
+    
+    # Transform
+
+    def transform_category_to_order(self, feature, order):
+        key = "transform_category_to_order"
+        self.plan = self.__include_on_order_list(self.plan, key, feature, order)
+        return self
+
+
+    def transform_linear(self, feature, operation):
+        key = "transform_linear"
+        self.plan = self.__include_on_operation_list(self.plan, key, feature, operation)
         return self
